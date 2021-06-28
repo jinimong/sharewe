e8988e417e4b1c3973d6717ce546ee8c24efbde4
jwkim <phoongdeng@gmail.com>
2021-06-28 16:36:10 +0900

    feat: Add TodoDetail

---
+++ pages/todos/[id].tsx
@@ -0,0 +1,27 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+import { useTodoContext } from '../../contexts/TodoContext';
+
+const TodoDetail: React.FC = () => {
+  const {
+    query: { id },
+  } = useRouter();
+  const { todos } = useTodoContext();
+  if (!id) {
+    return <div>404</div>;
+  }
+  const todo = todos.find((todo) => +id === todo.id);
+  if (!todo) {
+    return <div>404</div>;
+  }
+
+  return (
+    <div className="w-screen h-screen flex justify-center items-center text-center">
+      <div className="text-xl">
+        {todo.id}: {todo.text}
+      </div>
+    </div>
+  );
+};
+
+export default TodoDetail;
